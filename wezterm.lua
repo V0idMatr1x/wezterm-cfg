2cea7f47cb82d180159da5cc8dcf0e6b516ddb7f
V0idMatr1x <88049272+V0idMatr1x@users.noreply.github.com>
2022-05-04 22:50:50 +0000

    Update wezterm.lua

--- wezterm.lua
+++ wezterm.lua
@@ -18,7 +18,7 @@ return {
     font = wezterm.font('Dank Mono'),
     font_size = 16.0,
   
-    -- Disable ligatures
+    -- Enable ligatures
     -- https://wezfurlong.org/wezterm/config/font-shaping.html
     harfbuzz_features = { 'calt=0', 'clig=0', 'liga=1' },
   
